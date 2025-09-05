cf3e2252ac6631f51b8f0bf884225a59e5d851e7
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-09-05 14:47:37 +0800

    01_database: Continue practicing simple SQL queries on a single table.

--- 01_database/03_SQLZoo/sql_zoo.sql
+++ 01_database/03_SQLZoo/sql_zoo.sql
@@ -89,3 +89,24 @@ SELECT yr, subject FROM nobel WHERE winner = 'Albert Einstein';
 
 --Give the name of the 'peace' winners since the year 2000, including 2000.
 SELECT winner FROM nobel WHERE subject = 'Peace' AND yr >= 2000;
+
+--Show all details (yr, subject, winner) of the literature prize winners for 1980 to 1989 inclusive.
+SELECT yr, subject, winner FROM nobel WHERE subject = 'literature' AND yr  BETWEEN 1980 AND 1989;
+
+--Show all details of the presidential winners:Theodore Roosevelt Thomas Woodrow WilsonJimmy Carter Barack Obama
+SELECT * FROM nobel WHERE winner IN ('Theodore Roosevelt', 'Thomas Woodrow Wilson', 'Jimmy Carter', 
+'Barack Obama');
+
+--Show the winners with first name John
+SELECT winner FROM nobel WHERE winner LIKE 'John %';
+
+--Show the year, subject, and name of physics winners for 1980 together with the chemistry winners for 1984.
+SELECT yr, subject, winner FROM nobel WHERE
+(subject = 'Physics' AND yr = 1980 ) OR ( subject = 'chemistry' AND yr= 1984 );
+
+--Show the year, subject, and name of winners for 1980 excluding chemistry and medicine
+SELECT * FROM nobel WHERE yr = 1980 AND subject NOT IN ('chemistry', 'medicine');
+
+--Show year, subject, and name of people who won a 'Medicine' prize in an early year (before 1910, not including 1910) together with winners of a 'Literature' prize in a later year (after 2004, including 2004)
+SELECT yr, subject, winner FROM nobel WHERE 
+(subject = 'medicine' AND yr < 1910) OR ( subject = 'Literature' AND yr >= 2004);
