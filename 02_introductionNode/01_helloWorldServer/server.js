3f2c5ad058eedbda521ad0c240ec9522f754711a
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-09-06 21:35:51 +0800

    02_introductionNode: Recreating the Hello World Server.

--- 02_introductionNode/01_helloWorldServer/server.js
+++ 02_introductionNode/01_helloWorldServer/server.js
@@ -1,10 +1,32 @@
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "text/plain" });
+//   res.end("Hello World from Node.js");
+// });
+
+// server.listen(3000, () => {
+//   console.log("Server is running at http://localhost:3000");
+// });
+
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "text/plain" });
+//   res.end("Hello World to you User");
+// });
+
+// server.listen(3000, () => {
+//   console.log("Server is running at http://localhost:3000");
+// });
+
 const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World from Node.js");
+  res.end("Hello World again User...");
 });
 
 server.listen(3000, () => {
-  console.log("Server is running at http://localhost:3000");
+  console.log("Server is runnint at http://localhost:3000");
 });
