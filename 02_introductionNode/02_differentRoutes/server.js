30acc13d2800db2944e718f89e8c9c2620f779e4
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-09-07 09:40:07 +0800

    02_intorductionNode: Recalling and recreating the Dynamic Server Route.

--- 02_introductionNode/02_differentRoutes/server.js
+++ 02_introductionNode/02_differentRoutes/server.js
@@ -1,18 +1,37 @@
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     res.writeHead(200, { "Content-Type": "text/plain" });
+//     res.end("Welcome to the Home Page");
+//   } else if (req.url === "/about") {
+//     res.writeHead(200, { "Content-Type": "text/plain" });
+//     res.end("Welcome to the About Page");
+//   } else {
+//     res.writeHead(404, { "Content-Type": "text/plain" });
+//     res.end("404 Page not found");
+//   }
+// });
+
+// server.listen(3000, () => {
+//   console.log("Server is running on port http://localhost:3000");
+// });
+
 const http = require("http");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Welcome to the Home Page");
+    res.end("Welcome to Home Page");
   } else if (req.url === "/about") {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Welcome to the About Page");
   } else {
     res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("404 Page not found");
+    res.end("404, Page Not Found");
   }
 });
 
 server.listen(3000, () => {
-  console.log("Server is running on port http://localhost:3000");
+  console.log("Server is Running at http://localhost:3000");
 });
