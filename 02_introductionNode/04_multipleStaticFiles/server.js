464398ff9775d7aee3f3bb2642046cffd6ec5b60
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-09-07 22:05:10 +0800

    02_introductionNode: Recreating the multiple static server.

--- 02_introductionNode/04_multipleStaticFiles/server.js
+++ 02_introductionNode/04_multipleStaticFiles/server.js
@@ -1,3 +1,32 @@
+// const http = require("http");
+// const fs = require("fs");
+// const path = require("path");
+
+// const server = http.createServer((req, res) => {
+//   let filePath = req.url === "/" ? "/index.html" : req.url;
+//   let fullPath = path.join(__dirname, "public", filePath);
+
+//   fs.readFile(fullPath, (err, data) => {
+//     if (err) {
+//       res.writeHead(404, { "Content-Type": "text/plain" });
+//       res.end("File Not Found!");
+//     } else {
+//       const ext = path.extname(fullPath);
+//       let contentType = "text/html";
+
+//       if (ext === ".css") contentType = "text/css";
+//       if (ext === ".js") contentType = "application/javascript";
+
+//       res.writeHead(200, { "Content-Type": contentType });
+//       res.end(data);
+//     }
+//   });
+// });
+
+// server.listen(3000, () => {
+//   console.log("Server is Running at http://localhost:3000");
+// });
+
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
@@ -9,13 +38,14 @@ const server = http.createServer((req, res) => {
   fs.readFile(fullPath, (err, data) => {
     if (err) {
       res.writeHead(404, { "Content-Type": "text/plain" });
-      res.end("File not found...");
+      res.end("File Not Found!");
     } else {
       const ext = path.extname(fullPath);
       let contentType = "text/html";
 
       if (ext === ".css") contentType = "text/css";
-      if (ext === "js") contentType = "application/javascript";
+      if (ext === ".js") contentType = "application/javascript";
+
       res.writeHead(200, { "Content-Type": contentType });
       res.end(data);
     }
@@ -23,5 +53,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+  console.log("Server is running http://localhost:3000");
 });
