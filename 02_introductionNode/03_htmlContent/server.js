23adea0830d16a4a64e4ae6b5039c41f3b3d5409
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-09-07 09:56:17 +0800

    02_introductionNode: Creating a Server that returns an HTML Content.

---
+++ 02_introductionNode/03_htmlContent/server.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile("index.html", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Error loading page");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server is running at http://localhost:3000");
+});
