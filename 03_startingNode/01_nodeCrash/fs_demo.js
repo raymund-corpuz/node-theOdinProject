345a3f2b6f2242ee13f2e93403507afafcb9c7a9
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-09-08 20:30:02 +0800

    03_startingNode: Appending a new text to Hello.txt file .

--- 03_startingNode/01_nodeCrash/fs_demo.js
+++ 03_startingNode/01_nodeCrash/fs_demo.js
@@ -16,3 +16,23 @@ fs.writeFile(
     console.log("File Written ");
   }
 );
+
+//File Append
+
+// fs.appendFile(
+//   path.join(__dirname, "/test", "hello.txt"),
+//   " I Love JavaScript",
+//   (err) => {
+//     if (err) throw err;
+//     console.log("File is written to...");
+//   }
+// );
+
+fs.appendFile(
+  path.join(__dirname, "/test", "hello.txt"),
+  " I Love Node Js",
+  (err) => {
+    if (err) throw err;
+    console.log("File is written to...");
+  }
+);
