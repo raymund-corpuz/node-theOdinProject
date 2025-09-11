b839983699f20b5fed3892a3c157786263edeef2
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-09-11 13:33:23 +0800

    03_startingNode: Recreating the todoControllers adding try and catch.

---
+++ 03_startingNode/02_netNinja/12_wrapUp/02_todoApp/controllers/todoController.js
@@ -0,0 +1,80 @@
+// const Todo = require("../models/todoModel");
+
+// //show All todos
+// exports.getTodos = async (req, res) => {
+//   const todos = await Todo.find();
+//   res.render("index", { todos });
+// };
+
+// //add new todo
+// exports.addTodo = async (req, res) => {
+//   const { task } = req.body;
+
+//   if (!task) return res.redirect("/");
+
+//   await Todo.create({ task });
+//   res.redirect("/");
+// };
+
+// exports.deleteTodo = async (req, res) => {
+//   await Todo.findByIdAndDelete(req.params.id);
+//   res.redirect("/");
+// };
+
+// const Todo = require("../models/todoModel");
+
+// //show all todos
+// exports.getTodos = async (req, res) => {
+//   const todos = await Todo.find();
+//   res.render("index", { todos });
+// };
+
+// exports.addTodo = async (req, res) => {
+//   const { task } = req.body;
+
+//   if (!task) return res.redirect("/");
+
+//   await Todo.create({ task });
+//   res.redirect("/"); //missing
+// };
+
+// exports.deleteTodo = async (req, res) => {
+//   const deleted = await Todo.findByIdAndDelete(req.params.id);
+
+//   res.redirect("/");
+// };
+
+const Todo = require("../models/todoModel");
+
+exports.getTodos = async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.render("index", { todos });
+  } catch (err) {
+    console.error("Error fetching todos: ", err);
+    res.status(500).send("Server error");
+  }
+};
+
+exports.addTodo = async (req, res) => {
+  try {
+    const { task } = req.body;
+
+    if (!task) return res.redirect("/");
+    await Todo.create({ task });
+    res.redirect("/");
+  } catch (err) {
+    console.error("Error adding todo:", err);
+    res.status(500).send("Server error");
+  }
+};
+
+exports.deleteTodo = async (req, res) => {
+  try {
+    const deleted = await Todo.findByIdAndDelete(req.params.id);
+    res.redirect("/");
+  } catch (err) {
+    console.error("Error deleting", err);
+    res.status(500).send("Server error");
+  }
+};
