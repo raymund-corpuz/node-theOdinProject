8e036baf13f60cff83bb886801e9c9b313a511f7
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-09-12 20:00:05 +0800

    03_startingNode: Update the data.txt using the fs.writeFile.

--- 03_startingNode/02_netNinja/02_nodeBasics/01_fs/module.js
+++ 03_startingNode/02_netNinja/02_nodeBasics/01_fs/module.js
@@ -1,9 +1,19 @@
 const fs = require("fs");
 
-fs.readFile("./someData/data.txt", (err, data) => {
+//Read Files
+// fs.readFile("./someData/data.txt", (err, data) => {
+//   if (err) {
+//     console.error("Can't read the data.", err);
+//   }
+
+//   console.log(data.toString());
+// });
+
+//Write Files
+fs.writeFile("./someData/data.txt", "hello, Ninjas", (err) => {
   if (err) {
-    console.error("Can't read the data.", err);
+    console.error("Can't write inside the file");
   }
 
-  console.log(data.toString());
+  console.log("Data is Updated!");
 });
