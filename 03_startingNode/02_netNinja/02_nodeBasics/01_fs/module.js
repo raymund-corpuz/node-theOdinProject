bac26623292bab698ce164a050e985b056c11400
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-09-12 20:09:00 +0800

    03_startingNode: Adding and deleting folders using the mkdir and rmdir.

--- 03_startingNode/02_netNinja/02_nodeBasics/01_fs/module.js
+++ 03_startingNode/02_netNinja/02_nodeBasics/01_fs/module.js
@@ -10,10 +10,29 @@ const fs = require("fs");
 // });
 
 //Write Files
-fs.writeFile("./someData/data.txt", "hello, Ninjas", (err) => {
-  if (err) {
-    console.error("Can't write inside the file");
-  }
+// fs.writeFile("./someData/data.txt", "hello, Ninjas", (err) => {
+//   if (err) {
+//     console.error("Can't write inside the file");
+//   }
 
-  console.log("Data is Updated!");
-});
+//   console.log("Data is Updated!");
+// });
+
+//delete files
+
+if (!fs.existsSync("./assets")) {
+  fs.mkdir("./assets", (err) => {
+    if (err) {
+      console.error(`Can't create a folder`, err);
+    }
+
+    console.log("Folder is created");
+  });
+} else {
+  fs.rmdir("./assets", (err) => {
+    if (err) {
+      console.error(`Can't delete the folder`, err);
+    }
+    console.log(" Folder is removed");
+  });
+}
