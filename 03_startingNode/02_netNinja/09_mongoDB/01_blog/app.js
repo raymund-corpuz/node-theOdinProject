172e7e1a3b6f79758f9b6f2384806b03a33c5829
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-09-15 09:43:57 +0800

    03_startingNode: Creating a Blog Website using the Express framework.

---
+++ 03_startingNode/02_netNinja/09_mongoDB/01_blog/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+//register view engine
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile("./views/contact.html", { root: __dirname });
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
+});
+
+app.listen(8080, () => {
+  console.log(`Server is running at http://localhost:8080`);
+});
