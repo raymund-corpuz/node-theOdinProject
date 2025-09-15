d413a8d4837b1d2c63e778c1aae190ca72151781
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-09-15 10:41:30 +0800

    03_startingNode: Rendering the data dynamically.

--- 03_startingNode/02_netNinja/09_mongoDB/01_blog/app.js
+++ 03_startingNode/02_netNinja/09_mongoDB/01_blog/app.js
@@ -9,19 +9,34 @@ app.set("view engine", "ejs");
 //////////////////////
 
 app.get("/", (req, res) => {
-  res.render("index");
+  const blogs = [
+    {
+      title: "Youshi finds eggs",
+      snippet: "Lorem ispsum folor sit amet consectetur",
+    },
+    {
+      title: "Mario finds star",
+      snippet: "Lorem ispsum folor sit amet consectetur",
+    },
+    {
+      title: "How to defeat browser",
+      snippet: "Lorem ispsum folor sit amet consectetur",
+    },
+  ];
+
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", { title: "About" });
 });
 
 app.get("/blogs/create", (req, res) => {
-  res.render("create");
+  res.render("create", { title: "Create a new Blog" });
 });
 
 app.use((req, res) => {
-  res.render("404");
+  res.render("404", { title: "404" });
 });
 
 app.listen(8080, () => {
