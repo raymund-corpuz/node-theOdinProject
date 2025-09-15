ab1971e4cf153b26334f6b953f11570a9f955467
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-09-15 10:13:39 +0800

    03_startingNode : Creating the GUI for the Blog website using EJS templating.

--- 03_startingNode/02_netNinja/09_mongoDB/01_blog/app.js
+++ 03_startingNode/02_netNinja/09_mongoDB/01_blog/app.js
@@ -4,22 +4,46 @@ const app = express();
 //register view engine
 app.set("view engine", "ejs");
 
+////////////////////////
+//Render EJS FILES   //
+//////////////////////
+
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
 });
 
-app.get("/contact", (req, res) => {
-  res.sendFile("./views/contact.html", { root: __dirname });
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
 });
 
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.render("404");
 });
 
 app.listen(8080, () => {
   console.log(`Server is running at http://localhost:8080`);
 });
+
+//////////////////////////
+// Render HTML FILES   //
+////////////////////////
+// app.get("/", (req, res) => {
+//     res.sendFile("./views/index.html", { root: __dirname });
+//   res.render("index");
+// });
+
+// app.get("/about", (req, res) => {
+//   res.sendFile("./views/about.html", { root: __dirname });
+// });
+
+// app.get("/contact", (req, res) => {
+//   res.sendFile("./views/contact.html", { root: __dirname });
+// });
+
+// app.use((req, res) => {
+//   res.status(404).sendFile("./views/404.html", { root: __dirname });
+// });
