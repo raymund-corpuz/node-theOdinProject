9d3e7d3d1828b1dad7c8fb7a776c709617d7f02a
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-09-15 15:27:11 +0800

    03_startingNode: Adding new Blog using the server going through the mongoDB.

--- 03_startingNode/02_netNinja/09_mongoDB/01_blog/app.js
+++ 03_startingNode/02_netNinja/09_mongoDB/01_blog/app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 //connect to mongoDB
 const dB_URI =
-  "mongodb+srv://raymundcorpuz86_db_user:ray123456789@nodetuts.7pmkm7g.mongodb.net/?retryWrites=true&w=majority&appName=nodetuts";
+  "mongodb+srv://raymundcorpuz86_db_user:ray123456789@nodetuts.7pmkm7g.mongodb.net/node-tuts?retryWrites=true&w=majority&appName=nodetuts";
 
 mongoose
   .connect(dB_URI)
@@ -26,6 +27,44 @@ app.set("view engine", "ejs");
 //Render EJS FILES   //
 //////////////////////
 
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "Harley Butter",
+    snippet: "About Harley",
+    body: "Harley Butter and the Philosopher Stone",
+  });
+
+  blog
+    .save()
+    .then((results) => {
+      res.send(results);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/all-blog", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/single-blog", (req, res) => {
+  Blog.findById("68c7bd60bcb1fa3bfd404fc6")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+//middlewares
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
