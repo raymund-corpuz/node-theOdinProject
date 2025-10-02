f0fbd42925a4281be808e686f24fe7d50b51caa7
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-10-02 10:27:27 +0800

    12_postgres: Creating and testing the userModel.

---
+++ 12_postgres/02_mvcBook-v1/test.js
@@ -0,0 +1,31 @@
+const pool = require("./db/pool");
+const { findUserByUsername, createUser } = require("./models/userModel");
+
+async function test() {
+  const { rows } = await pool.query("SELECT * FROM books");
+  console.log(rows);
+  process.exit();
+}
+
+async function findUser() {
+  const USERNAME = "ray";
+  const user = await findUserByUsername(USERNAME);
+
+  console.log(user);
+}
+
+findUser();
+
+async function cUser() {
+  const NEW_USER = "ray";
+  const PASSWORD = "qwerty123";
+  await createUser(NEW_USER, PASSWORD);
+
+  console.log("✅ User successfully added");
+
+  const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [
+    NEW_USER,
+  ]);
+
+  console.log(rows);
+}
