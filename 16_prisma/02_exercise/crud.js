7a3e3e76f5d2ea320ea1bddc73b580231c081adc
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-10-13 20:17:56 +0800

    16_prisma: Re-write the prisma crud.

--- 16_prisma/02_exercise/crud.js
+++ 16_prisma/02_exercise/crud.js
@@ -1,38 +1,79 @@
-const { PrismaClient, Prisma } = require("@prisma/client");
+// const { PrismaClient, Prisma } = require("@prisma/client");
+// const prisma = new PrismaClient();
+
+// async function runCrud() {
+//   try {
+//     const newUser = await prisma.user.create({
+//       data: {
+//         name: "John Doe",
+//         email: "john@example.com",
+//       },
+//     });
+//     console.log("✅ Created :", newUser);
+
+//     //Read (all users)
+
+//     const users = await prisma.user.findMany();
+//     console.log("All Users :", users);
+
+//     //Update (by id)
+//     const updated = await prisma.user.update({
+//       where: { id: newUser.id },
+//       data: { name: "Johnny Updated" },
+//     });
+//     console.log("Updated : ", updated);
+
+//     //Delete (by id)
+//     const deleted = await prisma.user.delete({
+//       where: { id: newUser.id },
+//     });
+//     console.log("🗑️ Deleted :", deleted);
+//   } catch (err) {
+//     if (err.code === "P2002") {
+//       console.error("Unique constraint failed :", err.meta);
+//     } else {
+//       console.error("❌ Error :", err);
+//     }
+//   } finally {
+//     await prisma.$disconnect();
+//   }
+// }
+
+// runCrud();
+
+// ============= Re-Create =================== //
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function runCrud() {
   try {
     const newUser = await prisma.user.create({
       data: {
-        name: "John Doe",
-        email: "john@example.com",
+        name: "Raymund",
+        email: "ray@xexample.com",
       },
     });
-    console.log("✅ Created :", newUser);
-
-    //Read (all users)
+    console.log("✅ New User is Created :", newUser);
 
     const users = await prisma.user.findMany();
-    console.log("All Users :", users);
+    console.log("All users :", users);
 
-    //Update (by id)
     const updated = await prisma.user.update({
       where: { id: newUser.id },
-      data: { name: "Johnny Updated" },
+      data: { name: "Raymanxikoy" },
     });
-    console.log("Updated : ", updated);
 
-    //Delete (by id)
+    console.log("✅ Updated :", updated);
+
     const deleted = await prisma.user.delete({
       where: { id: newUser.id },
     });
-    console.log("🗑️ Deleted :", deleted);
-  } catch (err) {
-    if (err.code === "P2002") {
-      console.error("Unique constraint failed :", err.meta);
+    console.log("✅ Deleted :", deleted);
+  } catch (error) {
+    if (error === "P2002") {
+      console.error("Unique constraint failed :", error.meta);
     } else {
-      console.error("❌ Error :", err);
+      console.error("❌ Error", error);
     }
   } finally {
     await prisma.$disconnect();
